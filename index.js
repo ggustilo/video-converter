ed4500f7d509d7ccc56e8fa6b618b7afef61b4ba
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2018-06-05 22:30:11 -0500

    Update convert and add compression

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const convert = require('./lib/convert_format');
+const config = require('config'),
+	convert = require('./lib/convert_format'),
+	compress = require('./lib/compress');
 
-convert();
+const original_file_path = config.get('original_file_path'),
+	reformat_output_path = config.get('reformat_output_path'),
+	compressed_output_path = config.get('compressed_output_path'),
+	sliced_output_path = config.get('sliced_output_path'),
+	renamed_output_path = config.get('renamed_output_path');
+
+// convert(original_file_path, reformat_output_path);
+compress(reformat_output_path, compressed_output_path);
