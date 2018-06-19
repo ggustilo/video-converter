d3c7c567960c080694ecd78860af5ca0289fddcd
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2018-06-18 19:23:30 -0500

    ffprobe analysis working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('config'),
 	convert = require('./lib/convert_format'),
-	compress = require('./lib/compress');
+	compress = require('./lib/compress'),
+	slice_videos = require('./lib/slice_videos');
 
 const original_file_path = config.get('original_file_path'),
 	reformat_output_path = config.get('reformat_output_path'),
@@ -9,4 +10,5 @@ const original_file_path = config.get('original_file_path'),
 	renamed_output_path = config.get('renamed_output_path');
 
 // convert(original_file_path, reformat_output_path);
-compress(reformat_output_path, compressed_output_path);
+// compress(reformat_output_path, compressed_output_path);
+slice_videos(compressed_output_path, sliced_output_path);
